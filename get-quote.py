d48cd232db9f58da7a6aa8e76c8828adbc8688c4
Agustin Nolasco <franconolasco99@gmail.com>
2021-08-31 20:21:51 -0300

    Solved the last proposed exercises

--- get-quote.py
+++ get-quote.py
@@ -3,13 +3,19 @@ import random
 def main():
   #print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  f.write("I'm learning how to put a new line is a file using Python!")
+  f.close()
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd1].strip())
+  print(quotes[rnd2].strip())
 
 if __name__== "__main__":
   main()
